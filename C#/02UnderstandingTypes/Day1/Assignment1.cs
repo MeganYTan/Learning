bcebf90d9e7d17235c0b15c366b6bbba0b4b0ce6
Megan Tan <meganytan@gmail.com>
2024-10-09 01:37:35 -0500

    Add C# day 1

---
+++ C#/02UnderstandingTypes/Day1/Assignment1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+
+namespace _02UnderstandingTypes
+{
+    class Types
+    {
+        public static void PrintTypes()
+        {
+            
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "Type", "Size (bytes)", "Min Value", "Max Value");
+            Console.WriteLine(new string('-', 90));  
+            // sbyte
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+            // byte
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+            // short
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "short", sizeof(short), short.MinValue, short.MaxValue);
+            // ushort
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+            // int
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "int", sizeof(int), int.MinValue, int.MaxValue);
+            // uint
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+            // long
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "long", sizeof(long), long.MinValue, long.MaxValue);
+            // ulong
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+            // float
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "float", sizeof(float), float.MinValue, float.MaxValue);
+            // double
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "double", sizeof(double), double.MinValue, double.MaxValue);
+            // decimal
+            Console.WriteLine("{0, -10} {1, 15} {2, 30} {3, 30}", "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+            
+        }
+    }
+
+    class CenturyConverter
+    {
+        private static void ConvertCenturies(uint centuries)
+        {
+            // years = * 100
+            uint years = centuries * 100;
+            // days
+            ulong days = (ulong)(years * 365.24); // taking leap years into account
+            // hours
+            ulong hours = days * 24;
+            // minutes
+            ulong minutes = hours * 60;
+            // seconds
+            ulong seconds = minutes * 60;
+            // milliseconds
+            ulong milliseconds = seconds * 1000;
+            // microseconds
+            ulong microseconds = milliseconds * 1000;
+            // nanoseconds
+            decimal nanoseconds = microseconds * 1000;
+            Console.WriteLine($"Output: {centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds ");
+        }
+
+        public static void GetNumberOfCenturiesAndConvert()
+        {
+            Console.Write("Input: ");
+            uint numberOfCenturies = uint.Parse(Console.ReadLine());
+            ConvertCenturies(numberOfCenturies);
+        }
+    }
+
+    class FizzBuzz
+    {
+        public static void ExecuteFizzBuzz()
+        {
+            for (byte i = 0; i < 100; i++)
+            {
+                bool isFizzBuzz = false;
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                    isFizzBuzz = true;
+                }
+
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                    isFizzBuzz = true;
+                }
+
+                if (!isFizzBuzz)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+                
+            }
+        }
+
+        public static void CountTo500WithByte()
+        {
+            int max = 500;
+            for (byte i = 0; i < max; i++)
+            {
+                Console.WriteLine(i);
+                
+                if (i == 255)
+                {
+                    Console.WriteLine("Warning: 'i' will overflow after this point!");
+                }
+            }
+        }
+    }
+
+    class RandomNumber
+    {
+        public static void PlayGuessingGame()
+        {
+            int guessedNumber = 0;
+            while (guessedNumber < 1 || guessedNumber > 3)
+            {
+                Console.Write("Enter a number between 1 and 3: ");
+                guessedNumber = int.Parse(Console.ReadLine());
+            }
+            int correctNumber = new Random().Next(3) + 1;
+            if (correctNumber > guessedNumber)
+            {
+                Console.WriteLine("You guessed too low.");
+            }
+            else if (correctNumber < guessedNumber)
+            {
+                Console.WriteLine("You guessed too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed right!");
+            }
+        }
+
+    }
+
+    class Pyramid
+    {
+        public static void PrintPyramid(int lines = 5)
+        {
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < lines-1-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class BirthDaysOld
+    {
+        public static void CalculateBirthDaysOld(DateTime birthDate = default)
+        {
+            if (birthDate == default)
+            {
+                birthDate = new DateTime(1990, 1, 1);
+            }
+            
+            DateTime currentDate = DateTime.Now;
+            
+            TimeSpan ageInDays = currentDate - birthDate;
+            int totalDaysOld = (int)ageInDays.TotalDays;
+            
+            Console.WriteLine($"You are {totalDaysOld} days old.");
+            
+            int daysToNextAnniversary = 10000 - (totalDaysOld % 10000);
+            DateTime nextAnniversaryDate = currentDate.AddDays(daysToNextAnniversary);
+            
+            Console.WriteLine($"Your next 10,000-day anniversary will be in {daysToNextAnniversary} days, on {nextAnniversaryDate.ToShortDateString()}.");
+        }
+    }
+
+    class Greeting
+    {
+        public static void GreetUser(DateTime time = default)
+        {
+            if (time == default)
+            {
+                time = DateTime.Now;
+            }
+            int hour = time.Hour;
+            
+            if (hour >= 5 && hour < 12)
+            {
+                Console.WriteLine("Good Morning");
+            }
+            if (hour >= 12 && hour < 17)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            if (hour >= 17 && hour < 21)
+            {
+                Console.WriteLine("Good Evening");
+            }
+            if (hour >= 21 || hour < 5)
+            {
+                Console.WriteLine("Good Night");
+            }
+        }
+    }
+
+    class Counting
+    {
+        public static void CountTo24()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 0; j <= 24; j=j+i)
+                {
+                    Console.Write(j);
+                    if (j < 24)
+                    {
+                        Console.Write(",");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+    class Assignment1
+    {
+        public static void Main(string[] args)
+        {
+            
+            // Practice number sizes and ranges
+            // Q1 print types
+            Console.WriteLine("Print Types");
+            Types.PrintTypes();
+            // Q2 Convert centuries
+            Console.WriteLine("Convert Centuries");
+            CenturyConverter.GetNumberOfCenturiesAndConvert();
+            
+            // Controlling Flow and Converting Types
+            // Q1 Fizz Buzz
+            Console.WriteLine("Fizz Buzz");
+            FizzBuzz.ExecuteFizzBuzz();
+            // Q2 Print-a-Pyramid
+            Console.WriteLine("Print-a-Pyramid");
+            Pyramid.PrintPyramid();
+            // Q3 Guesssing game
+            Console.WriteLine("Guessing Game");
+            RandomNumber.PlayGuessingGame();
+            // Q4 Calculate how many days old a person is
+            Console.WriteLine("Calculate Birth Days Old");
+            BirthDaysOld.CalculateBirthDaysOld(new DateTime(1997,5,16));
+            // Q5 Greet User
+            Console.WriteLine("Greet user based on time");
+            Greeting.GreetUser();
+            // Q6 Counting to 24 in increments
+            Console.WriteLine("Count to 24 in increments");
+            Counting.CountTo24();
+            
+            
+            // Assignment 2 PracticeArray
+            Console.WriteLine("Assignment 2 - PracticeArray");
+            // Q1 - Copy Array
+            Console.WriteLine("Copy Array");
+            PracticeArrays.CopyArray();
+            // Q2 - List Manager
+            Console.WriteLine("List Manager");
+            PracticeArrays.ListManager();
+            // Q3 - Print Prime Numbers
+            Console.WriteLine("Find primes in range");
+            PracticeArrays.PrintArray(PracticeArrays.FindPrimesInRange(5,60));
+            // Q4 - Rotate Array and Sum
+            Console.WriteLine("Rotate array and sum");
+            PracticeArrays.RotateArray();
+            // Q5 - Longest Sequence of Equal Elements
+            Console.WriteLine("Get longest sequence of equal elements");
+            PracticeArrays.LongestSequenceOfEqualElements([0,1,1,5,2,2,6,3,3]);
+            // Q7 Most Frequest Number
+            Console.WriteLine("Get most frequent number");
+            PracticeArrays.MostFrequentNumber([7,7,7,0,2,2,2,0,10,10,10]);
+            
+            // PracticeStrings
+            // Q1 - Reverse Strings
+            Console.WriteLine("PracticeStrings");
+            Console.WriteLine("Reverse Strings");
+            PracticeStrings.ReverseString("sample");
+            // Q2 - Reverse Words In Sentence
+            Console.WriteLine("Reverse Words in Sentence");
+            PracticeStrings.ReverseWordsInSentence("The quick brown fox jumps over the lazy dog /Yes! Really!!!/.");
+            // Q3 - Get palindroms
+            Console.WriteLine("Get palindroms in sentence");
+            PracticeStrings.GetPalindromes("Hi, exe? ABBA! Hog fully a string: ExE. Bob");
+            // Q4 - Splitting URL
+            Console.WriteLine("Split url into protocol, server, and resource");
+            PracticeStrings.URLParser("https://google.com");
+        }
+    }
+}
---
+++ C#/02UnderstandingTypes/Day1/Assignment2.cs
@@ -0,0 +1,326 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02UnderstandingTypes;
+
+public class PracticeArrays
+{
+    public static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i != array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    public static void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i != array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+    public static void CopyArray()
+    {
+        int[] originalArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] newArray = new int[originalArray.Length];
+        for (byte i = 0; i < originalArray.Length; i++)
+        {
+            newArray[i] = originalArray[i];
+        }
+        Console.WriteLine("Original array:");
+        PrintArray(originalArray);
+        
+        Console.WriteLine("New array:");
+        PrintArray(newArray);
+    }
+
+    public static void ListManager()
+    {
+        List<string> itemList = new List<string>();
+        bool shouldLoop = true;
+        while (shouldLoop)
+        {
+            Console.WriteLine("Enter command (+ item, - item, or -- to clear, or q to exit)):");
+            string input = Console.ReadLine();
+            switch (input[0])
+            {
+                case '+': // Add an item to the list
+                    string itemToAdd = input.Substring(1).Trim();
+                    if (!string.IsNullOrEmpty(itemToAdd))
+                    {
+                        itemList.Add(itemToAdd);
+                        PrintArray(itemList.ToArray());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid item to add.");
+                    }
+
+                    break;
+
+                case '-': // Remove an item or clear the list
+                    if (input == "--")
+                    {
+                        itemList.Clear();
+                        Console.WriteLine("List cleared.");
+                    }
+                    else
+                    {
+                        string itemToRemove = input.Substring(1).Trim();
+                        if (itemList.Contains(itemToRemove))
+                        {
+                            itemList.Remove(itemToRemove);
+                            PrintArray(itemList.ToArray());
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Item not found: {itemToRemove}");
+                        }
+                    }
+
+                    break;
+                case 'q':
+                    shouldLoop = false;
+                    break;
+
+                default: // Invalid input
+                    Console.WriteLine("Invalid input. Use + to add, - to remove, or -- to clear.");
+                    break;
+            }
+        }
+    }
+
+    public static int[] FindPrimesInRange(int startNum, int endNum)
+    {
+        List<int> primes = new List<int>();
+        for (int i = startNum; i <= endNum; i++)
+        {
+            // if prime
+            bool isPrime = true;
+            for (int j = 2; j*j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                primes.Add(i);
+            }
+        }
+        return primes.ToArray();
+    }
+
+    public static void RotateArray()
+    {
+        Console.WriteLine("Enter the array of integers (space separated):");
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine("Enter the number of rotations:");
+        int k = int.Parse(Console.ReadLine());
+
+        int n = array.Length;
+        int[] sumArray = new int[n]; 
+
+        
+        for (int r = 1; r <= k; r++)
+        {
+            int[] rotatedArray = new int[n];
+            // rotate array
+            for (int i = 0; i < n; i++)
+            {
+                int newPosition = (i + r) % n;
+                rotatedArray[newPosition] = array[i];
+            }
+            // add it to sum array
+            for (int i = 0; i < n; i++)
+            {
+                sumArray[i] += rotatedArray[i];
+            }
+        }
+        Console.WriteLine("Sum array:");
+        Console.WriteLine(string.Join(" ", sumArray));
+    }
+
+    public static void LongestSequenceOfEqualElements(int[] array)
+    {
+        int currentNum = array[0];
+        int currentLength = 1;
+        int longestNum = currentNum;
+        int longestLength = currentLength;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] == currentNum)
+            {
+                currentLength++;
+            }
+            else
+            {
+                currentNum = array[i];
+                currentLength = 1;
+            }
+
+            if (currentLength > longestLength)
+            {
+                longestLength = currentLength;
+                longestNum = currentNum;
+                
+            }
+        }
+
+        for (int i = 0; i < longestLength; i++)
+        {
+            Console.Write(longestNum + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void MostFrequentNumber(int[] array)
+    {
+        Dictionary<int, int> frequencies = new Dictionary<int, int>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (!frequencies.ContainsKey(array[i]))
+            {
+                frequencies.Add(array[i], 1);
+            }
+            else
+            {
+                frequencies[array[i]]++;
+            }
+        }
+
+        int mostFrequentNumber = 0;
+        int frequency = 0;
+        foreach (KeyValuePair<int,int> keyValuePair in frequencies)
+        {
+            if (keyValuePair.Value > frequency)
+            {
+                mostFrequentNumber = keyValuePair.Key;
+                frequency = keyValuePair.Value;
+            }
+        }
+        Console.WriteLine($"Most frequent number is {mostFrequentNumber}");
+    }
+
+    
+}
+
+public class PracticeStrings
+{
+    public static void ReverseString(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        for (int i = 0; i < charArray.Length / 2; i++)
+        {
+            char temp = charArray[i];
+            charArray[i] = charArray[charArray.Length - 1 - i];
+            charArray[charArray.Length - 1 - i] = temp;
+        }
+        string reversedString = new string(charArray);
+        Console.WriteLine(reversedString);
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            Console.Write(s[i]);
+        }
+        Console.WriteLine();
+    }
+    
+    public static string ReverseWordsInSentence(string sentence)
+    {
+        string pattern = @"[\s.,:;=\(\)&\[\]""'\\\/!?]+";
+        List<string> words = new List<string>(Regex.Split(sentence, pattern));
+        words.RemoveAll(w => string.IsNullOrEmpty(w));
+        words.Reverse();
+        List<string> separators = new List<string>(Regex.Matches(sentence, pattern).Cast<Match>().Select(m => m.Value));
+
+        string CombineWordsAndSeparators(List<string> wordsList, List<string> separatorsList)
+        {
+            List<string> result = new List<string>();
+            int maxLength = Math.Max(wordsList.Count, separatorsList.Count);
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < wordsList.Count)
+                {
+                    result.Add(wordsList[i]);
+                }
+                if (i < separatorsList.Count)
+                {
+                    result.Add(separatorsList[i]);
+                }
+            }
+            return string.Join("", result);
+        }
+        string answer = CombineWordsAndSeparators(words, separators);
+        Console.WriteLine(answer);
+        return answer;
+    }
+
+    public static string[] GetPalindromes(string sentence)
+    {
+        bool IsPalindrome(string word)
+        {
+            int length = word.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (word[i] != word[length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        string[] words = Regex.Split(sentence, @"\W+");  
+        HashSet<string> palindromesSet = new HashSet<string>();
+        foreach (string word in words)
+        {
+            if (IsPalindrome(word))  
+            {
+                palindromesSet.Add(word);
+            }
+        }
+        string[] palindromesArray = palindromesSet.ToArray();
+        Array.Sort(palindromesArray);  
+        PracticeArrays.PrintArray(palindromesArray);
+        return palindromesArray;
+    }
+
+    public static void URLParser(string url)
+    {
+        string protocol = "";
+        if (url.Contains("://"))
+        {
+            int protocolIndex = url.IndexOf("://");
+            protocol = url.Substring(0, protocolIndex);
+            url = url.Substring(protocolIndex + 3);
+        }
+
+        string server = url;
+        string resource = "";
+        if (url.Contains("/"))
+        {
+            int slashIndex = url.IndexOf("/");
+            server = url.Substring(0, slashIndex);
+            resource = url.Substring(slashIndex+1);
+        }
+        Console.WriteLine($"[protocol] = \"{protocol}\"");
+        Console.WriteLine($"[server] = \"{server}\"");
+        Console.WriteLine($"[resource] = \"{resource}\"");
+    }
+}
+
